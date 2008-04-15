fdc6a455583bfda98dd034a263958e7e7e7ef8c2
tonywzt1 <tonywzt1>
2008-04-15 20:10:13 +0000

    *** empty log message ***

--- web/WEB-INF/classes/src/com/quatro/common/KeyConstants.java
+++ web/WEB-INF/classes/src/com/quatro/common/KeyConstants.java
@@ -2,6 +2,11 @@ package com.quatro.common;
 
 public class KeyConstants {
   public static final String SESSION_KEY_FACILITYID="currentFacilityId"; 
-  public static final String SESSION_KEY_PROVIDERNO="user"; 
-  //request.getSession().getAttribute(SESSION_KEY_PROVIDERNO)
+  public static final String SESSION_KEY_PROVIDERNO="user";
+  public static final String SESSION_KEY_SECURITY_MANAGER="secMgr";
+
+  public static final String CONSTANT_GENDER_Male = "M";
+  public static final String CONST_GENDER_Female = "F";
+  public static final String CONST_GENDER_Transgender = "T";
+
 }
