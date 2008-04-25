b10277180fd972270312edf3474352c3b3cf818d
lli_quatro <lli_quatro>
2008-04-25 13:01:12 +0000

    *** empty log message ***

--- web/WEB-INF/classes/src/com/quatro/common/KeyConstants.java
+++ web/WEB-INF/classes/src/com/quatro/common/KeyConstants.java
@@ -14,5 +14,7 @@ public class KeyConstants {
 
   public static final String CONSTANT_YES = "1";
   public static final String CONSTANT_NO = "0";
-
+  
+  public static final String SESSION_KEY_CLIENTID="clientId"; 
+  public static final String SESSION_KEY_CURRENT_FUNCTION="func"; 
 }
