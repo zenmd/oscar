b755a0f56deba0b8d10e970294b5b285024e1c0a
dawsonli <dawsonli>
2008-06-18 18:20:40 +0000

    *** empty log message ***

--- web/WEB-INF/classes/src/org/oscarehr/PMmodule/service/ClientRestrictionManager.java
+++ web/WEB-INF/classes/src/org/oscarehr/PMmodule/service/ClientRestrictionManager.java
@@ -138,6 +138,7 @@ public class ClientRestrictionManager {
 
     public boolean checkGenderConflict(Program program, Demographic demographic) {
       if (program.getManOrWoman()!=null && demographic.getSex()!=null){
+    	 if("T".equals(demographic.getSex())) return false;
          if ("M".equals(program.getManOrWoman()) && !"M".equals(demographic.getSex()))  return true;
          if ("F".equals(program.getManOrWoman()) && !"F".equals(demographic.getSex())) return true;
       }
